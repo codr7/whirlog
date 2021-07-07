16f812cf9b4869b82a31533abd40ee1786d2adde
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-07-08 00:57:31 +0200

    Added vector compare.

--- sort.lisp
+++ sort.lisp
@@ -4,6 +4,22 @@
 
 (in-package sort)
 
+(defmethod compare ((x list) y)
+  (declare (type list x y))
+  
+  (cond
+    ((and x (null y))
+     :gt)
+    ((and (null x) y)
+     :lt)
+    ((or (null x) (null y))
+     :eq)
+    (t
+     (let ((res (compare (first x) (first y))))
+       (if (eq res :eq)
+	   (compare (rest x) (rest y))
+	   res)))))
+
 (defmethod compare ((x number) y)
   (declare (type number x y))
   
@@ -24,18 +40,11 @@
   (declare (type symbol x y))
   (compare (symbol-name x) (symbol-name y)))
 
-(defmethod compare ((x list) y)
-  (declare (type list x y))
-  
-  (cond
-    ((and x (null y))
-     :gt)
-    ((and (null x) y)
-     :lt)
-    ((or (null x) (null y))
-     :eq)
-    (t
-     (let ((res (compare (first x) (first y))))
-       (if (eq res :eq)
-	   (compare (rest x) (rest y))
-	   res)))))
+
+(defmethod compare ((x vector) y)
+    (dotimes (i (min (length x) (length y)))
+      (ecase (compare (aref x i) (aref y i))
+	(:lt (return-from compare :lt))
+	(:gt (return-from compare :gt))
+	(:eq)))
+    :eq)
